3c1e66ed6c771d9ee430c5d1a4686510205ccea3
Barbi Torres <barbi.mtorres@gmail.com>
2023-02-02 13:13:13 +0100

    composicion y colocacion del estado compartido

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,47 @@
-import React from 'react';
-import { TodoProvider } from '../TodoContext';
-import { AppUI } from './AppUI';
-
+import React, { useContext, useState } from "react";
+import { TodoProvider } from "../TodoContext";
+import { AppUI } from "./AppUI";
 
 function App() {
+  const [state, setState] = useState("estado compartido desde APP");
+
   return (
-    <TodoProvider>
-      <AppUI />
-    </TodoProvider>
+    <>
+      <TodoHeader>
+        <TodoCounter />
+        <TodoSearch />
+      </TodoHeader>
+      <TodoList>
+        <TodoItem state={state} />
+      </TodoList>
+    </>
   );
+}
+
+function TodoHeader({ children }) {
+  //le mandamos la prop children para q se rendericen los componentes q queremos q vayan dentro
+  return (
+    <>
+      <header>{children}</header>
+    </>
+  );
+}
+
+function TodoList({ children }) {
+  //llamamos a la propiedad children de nuestro componente 
+  return <section className="TodoList-container">{children}</section>;
+}
+
+function TodoCounter() {
+  return <p>todocounter</p>;
+}
+
+function TodoSearch() {
+  return <p>TodoSearch</p>;
+}
+
+function TodoItem({state}) {
+  return <p>TodoItem: {state}</p>;
 }
 
 export default App;
