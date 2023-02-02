7f33c7da08cb0b32315531a90ef907cc8f1554d2
Barbi Torres <barbi.mtorres@gmail.com>
2023-02-02 13:42:32 +0100

    composicion de componentes con React Context

--- src/App/index.js
+++ src/App/index.js
@@ -3,45 +3,11 @@ import { TodoProvider } from "../TodoContext";
 import { AppUI } from "./AppUI";
 
 function App() {
-  const [state, setState] = useState("estado compartido desde APP");
-
   return (
-    <>
-      <TodoHeader>
-        <TodoCounter />
-        <TodoSearch />
-      </TodoHeader>
-      <TodoList>
-        <TodoItem state={state} />
-      </TodoList>
-    </>
+    <TodoProvider>
+      <AppUI />
+    </TodoProvider>
   );
-}
-
-function TodoHeader({ children }) {
-  //le mandamos la prop children para q se rendericen los componentes q queremos q vayan dentro
-  return (
-    <>
-      <header>{children}</header>
-    </>
-  );
-}
-
-function TodoList({ children }) {
-  //llamamos a la propiedad children de nuestro componente 
-  return <section className="TodoList-container">{children}</section>;
-}
-
-function TodoCounter() {
-  return <p>todocounter</p>;
-}
-
-function TodoSearch() {
-  return <p>TodoSearch</p>;
-}
-
-function TodoItem({state}) {
-  return <p>TodoItem: {state}</p>;
 }
 
 export default App;
